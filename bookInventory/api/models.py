65470aae9964d7cf28a1a475b667e4a12f52f8a6
bhudkg <42385281+bhudkg@users.noreply.github.com>
2024-07-07 15:32:45 +0530

    final commit

---
+++ bookInventory/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    OPTIONS = [
+        ("s", "Science"),
+        ("a", "Arts"),
+        ("f", "Fiction"),
+        ("d", "Drama"),
+    ]
+
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    category = models.CharField(max_length=10, choices=OPTIONS)
+
+    def __str__(self):
+        return self.name
---
+++ bookInventory/api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['GET'])
+def book_list(request):
+    query = Book.objects.all()
+    serializer = BookSerializer(query, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def book_single_view(request, id):
+    query = Book.objects.get(id=id)
+    serializer = BookSerializer(query)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def book_post(request):
+    serializer  = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['PUT'])
+def book_update(request, id):
+    query = Book.objects.get(id=id)
+    serializer  = BookSerializer(query, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def book_delete(request, id):
+    query = Book.objects.get(id=id)
+    query.delete()
+    return Response("Record has been deleted Successfully!!")
+
+
+
+    
+
